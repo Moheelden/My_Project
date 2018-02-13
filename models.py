64a158bc4fea1c83644c06d548ef138621649fa0
salimab <35033341+salimab@users.noreply.github.com>
2018-02-13 21:22:16 +0100

    Add files via upload

---
+++ models.py
@@ -0,0 +1,22 @@
+#-------------------------------------------------------------------------------
+# Name:        Models
+# Purpose:
+#
+# Author:      user
+#
+# Created:     10/02/2018
+# Copyright:   (c) user 2018
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+class Membre :
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
+
+class Post :
+    def __init__(self,title,topic):
+        self.title = title
+        self.topic = topic
+
+
+
