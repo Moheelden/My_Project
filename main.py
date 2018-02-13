d904533b6ee6fb8130bd5f9838772a9a7d70f485
salimab <35033341+salimab@users.noreply.github.com>
2018-02-13 21:36:58 +0100

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ membre2 = Membre(name2 , age2)
 print(str(membre1.name) + 5 * " " + " | " + str(membre1.age))
 print(str(membre2.name) + 5 * " " + " | " + str(membre2.age))
 
-post1 = Membre("title1" , "topic1")
+post1 = Post("title1" , "topic1")
 
 print( (post1.title) + 5 * " " + " | " + str(post1.topic) )
 
