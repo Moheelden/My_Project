2c1e3061367b25f86a7c9a6c6d81be802a9378f9
salimab <35033341+salimab@users.noreply.github.com>
2018-02-25 18:59:21 +0100

    Add files via upload

---
+++ flask_demo.py
@@ -0,0 +1,19 @@
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+@app.route("/index")
+
+def home():
+
+    return "Hi there"
+
+@app.route("/SayHello/<name>")
+
+def say_hello(name):
+
+    return "Hello {}".format(name)
+app.run()
+
